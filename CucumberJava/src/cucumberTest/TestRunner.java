1b5ca1e5fa3638dd8c17430519bd59fadc1bbb0e
dinadeken <30344188+dinadeken@users.noreply.github.com>
2022-05-08 12:55:10 +0200

    folders updated

--- CucumberJava/src/cucumberTest/TestRunner.java
+++ CucumberJava/src/cucumberTest/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features", glue= {"stepDefinition"})
+@CucumberOptions(features="Features", glue= {"stepDefinition"}, plugin = { "pretty","json:target/cucumber-reports/Cucumber.json","html:target/cucumber-reports/Cucumber.html" },
+monochrome = true)
 public class TestRunner {
 	
 	
--- CucumberJava/src/stepDefinition/TestSteps.java
+++ CucumberJava/src/stepDefinition/TestSteps.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.cucumber.java.en.Given;
@@ -14,12 +15,15 @@ import io.cucumber.java.en.When;
 
 public class TestSteps {
 	
-	FirefoxDriver driver = new FirefoxDriver();
+	 
+	
+	ChromeDriver driver = new ChromeDriver();
 	
 	
 	@Given("User in Login page")
 	public void user_in_login_page() {
 	
+		//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
 		driver.navigate().to("https://the-internet.herokuapp.com/login");
 		driver.manage().timeouts().implicitlyWait(120,TimeUnit.SECONDS);
 		
@@ -42,5 +46,19 @@ public class TestSteps {
 	  WebElement secureHeader = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h2"));
 	  Assert.assertTrue(secureHeader.isDisplayed());
 	}
+	
+	@Then("Message displayed your username is invalid")
+	public void message_displayed_your_username_is_invalid() {
+		WebElement errorDisplay = driver.findElement(By.className("flash error"));
+		Assert.assertTrue(errorDisplay.isDisplayed());
+	}
+	
+	@Then("Message displayed your password is invalid")
+	public void message_displayed_your_password_is_invalid() {
+	    // Write code here that turns the phrase above into concrete actions
+	 //   throw new io.cucumber.java.PendingException();
+	}
+
+	
 
 }
